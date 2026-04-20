3c42110cfac49170fcf68c558e2bba28ffb12dbc
Fabio Kusaba <fabinhokb44@gmail.com>
2026-04-19 21:00:44 -0300

    adicionar queries personalizadas para buscar todos os exercicios de um grupo muscular

--- src/main/java/dev/fabiokusaba/essentials/database/repository/IExercicioRepository.java
+++ src/main/java/dev/fabiokusaba/essentials/database/repository/IExercicioRepository.java
@@ -2,6 +2,24 @@ package dev.fabiokusaba.essentials.database.repository;
 
 import dev.fabiokusaba.essentials.database.model.ExercicioEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.NativeQuery;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface IExercicioRepository extends JpaRepository<ExercicioEntity, Long> {
+
+    // Query Method
+    List<ExercicioEntity> findAllByGrupoMuscular(String grupoMuscular);
+
+    @Query(value = "SELECT e FROM ExercicioEntity e WHERE UPPER(e.grupoMuscular) = UPPER(:grupoMuscular)")
+    List<ExercicioEntity> findAllByGrupoMuscularJpql(@Param("grupoMuscular") String grupoMuscular);
+
+    @NativeQuery(value = """
+        SELECT e
+        FROM exercicios e
+        WHERE UPPER(e.grupo_muscular) = UPPER(:grupoMuscular)
+    """)
+    List<ExercicioEntity> findAllByGrupoMuscularNative(@Param("grupoMuscular") String grupoMuscular);
 }
