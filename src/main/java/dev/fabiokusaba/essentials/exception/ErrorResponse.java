e23c7e4605c60c186799c6e94f6420c6f183f936
Fabio Kusaba <fabinhokb44@gmail.com>
2026-04-21 19:44:03 -0300

    adicionar camada de excecoes da aplicacao

---
+++ src/main/java/dev/fabiokusaba/essentials/exception/ErrorResponse.java
@@ -0,0 +1,4 @@
+package dev.fabiokusaba.essentials.exception;
+
+public record ErrorResponse(String message, Integer status) {
+}
---
+++ src/main/java/dev/fabiokusaba/essentials/handler/GlobalExceptionHandler.java
@@ -0,0 +1,34 @@
+package dev.fabiokusaba.essentials.handler;
+
+import dev.fabiokusaba.essentials.exception.BadRequestException;
+import dev.fabiokusaba.essentials.exception.ErrorResponse;
+import dev.fabiokusaba.essentials.exception.NotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleNotFoundException(NotFoundException ex) {
+        var response = new ErrorResponse(ex.getMessage(), HttpStatus.NOT_FOUND.value());
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+    }
+
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<ErrorResponse> handleBadRequestException(BadRequestException ex) {
+        var response = new ErrorResponse(ex.getMessage(), HttpStatus.BAD_REQUEST.value());
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleGenericException(Exception ex) {
+        var response = new ErrorResponse(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR.value());
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+    }
+}
